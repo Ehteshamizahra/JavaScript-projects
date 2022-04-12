ef8789d5cb7929d3b7eecd294ba95afcc73a2d8f
Zahra Ehteshami <ehteshamizahra@gmail.com>
2022-04-12 10:27:16 +0100

    added new comment to the js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
 function btn_func() {
-    var a="variable 1";
-    var b="variable 2";
-    document.getElementById("btn_text").innerHTML=a+" & "+b;
+    var a="variable 1"; //Define a variable with a string value
+    var b="variable 2";//Define a variable with a string value
+    document.getElementById("btn_text").innerHTML=a+" & "+b; //concatinate the string values on the page using event structure
 }
 function my_func() {
-    var sentence="<br>first sentence";
-    sentence += " & the second sentence";
-    document.getElementById("concatinate").innerHTML=sentence;
+    var sentence="<br>first sentence";//Define a variable with a string value
+    sentence += " & the second sentence";//concatinate a new string to the variable
+    document.getElementById("concatinate").innerHTML=sentence; //display the string on the page once the event is triggered
 }

a6de57cc79c730d576a39d66aa7230088c0cd67a
Zahra Ehteshami <ehteshamizahra@gmail.com>
2022-04-14 22:18:43 +0100

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,12 +1,18 @@
-var B="Welcome to my page"; //define a variable and give it a string value
+var B="Welcome to my page."; //define a variable and give it a string value
 window.alert(B);
 document.write("This is an example of \opostoph' and \?" 
 + " This is a concatinated string <br>"); //write a concatinated string value on the page
-var C="This is an example of concatinating strings"+"The second sentence."; //define a variable and give it a string value
-document.write(C+"<br>"); //write teh value of C on the page
+var A="This is the first sentence.";
+var C="This is an example of concatinating strings"; //define a variable and give it a string value
+var D=A+" "+C+"<br>";
+//D.FontColor("green");//.fontcolor is deprectaed
+document.write(D.fontcolor("green")); //write the value of C on the page
+
+
 var animals="big_cats", stripy="tiger", solid_dots="cheetah", rosettes="leopard", dots_rosettes="Jaguar", plain="lion";
 document.write(stripy+"<br>"); //define a variable and give it multiple string value
 document.write(8/2+"<br>"); //write an expression on the page
+
 function show_time(){
     a=Date();
     document.getElementById("button_event").innerHTML=a;

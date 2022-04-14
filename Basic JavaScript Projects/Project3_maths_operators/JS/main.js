9487d966130224f6fb22a458a824ff9217772f7c
Zahra Ehteshami <ehteshamizahra@gmail.com>
2022-04-14 23:12:51 +0100

    Added a new folder

---
+++ Basic JavaScript Projects/Project3_maths_operators/JS/main.js
@@ -0,0 +1,40 @@
+function my_func() { //Create a function for addition
+    var addition=2+2; //Define an operation and assign it to a variable
+    document.getElementById("maths").innerHTML="2+2="+addition; //Display the operation when the event is triggered 
+}
+function my_func2() {//Create a function for addition
+    var subtraction=3-5;//Define an operation and assign it to a variable
+    document.getElementById("maths2").innerHTML="3-5="+subtraction; //Display the operation when the event is triggered
+}
+function my_func3() {//Create a function for addition
+    var multi=2*2;//Define an operation and assign it to a variable
+    document.getElementById("maths3").innerHTML="2*2="+multi;//Display the operation when the event is triggered
+}
+function my_func4() {//Create a function for addition
+    var devision=3/5;//Define an operation and assign it to a variable
+    document.getElementById("maths4").innerHTML="3/5="+devision;//Display the operation when the event is triggered
+}
+function my_func5() {//Create a function for addition
+    var experession=(3/5)*2+6;//Define an operation and assign it to a variable
+    document.getElementById("maths5").innerHTML="3 devided 5, multiplied by 2, plus 6 equals "+ experession;//Display the operation when the event is triggered
+}
+function my_func6() {//Create a function for addition
+    var remainder=3%5;//Define an operation and assign it to a variable
+    document.getElementById("maths6").innerHTML="When you devide 3 by 5 the remainder is: "+ remainder;//Display the operation when the event is triggered
+}
+function my_func7() {//Create a function for addition
+    var x=10;//Define a variable
+    document.getElementById("maths7").innerHTML=-x;//Display the operation when the event is triggered
+}
+var x=10.85;//Define an operation and assign it to a variable
+x++;//Increment a variable
+document.write(x +",");//Display the operation
+
+var y=10;
+y--;//Decrement a variable
+document.write(y+",");//Display the operation
+
+document.write(Math.random()+",");//Display the operation
+
+document.write(Math.round(x)+",");
+

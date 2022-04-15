8ef3dba6b44bb09a9dac567c5d26f7b7fa65cc0a
Zahra Ehteshami <ehteshamizahra@gmail.com>
2022-04-15 18:20:40 +0100

    Added a new folder

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,36 @@
+document.write(typeof true + "<br>");//retun the type of a variable
+document.write(true + 6 + "<br>");//Example of type coercion
+document.write(0/0 + "<br>");//Display a NAN on the page
+document.write(isNaN("string") + "<br>");//Check if a variable is number, false is displayed if it's a number
+document.write(isNaN("369") + "<br>");//Check if a variable is number, false is displayed if it's a number
+document.write(2E310 + "<br>");//Display +ve infinity in the page
+document.write(-2E310 + "<br>");;//Display -ve infinity in the page
+document.write(2 < 11);//utilise smaller than >< to check if one variable is smaller than the other
+document.write("<br>");//Implement a line return
+document.write(2 > 11 );//utilise bigger than > to check if one variable is bigger than the other
+document.write("<br>");
+console.log(2+2);//Display an operation in the browser consol
+console.log(2>3);//Display a false return in the browser consol
+document.write(2 == 11 );////utilise == to check if two variable are equal
+document.write("<br>");
+document.write("2" == "2" );//utilise == to check if two variable are equal
+document.write("<br>");
+document.write("2" === "2" );//utilise === to check if the value and type of two variables are equal
+document.write("<br>");
+document.write("2" === 1 );//utilise === to check if the value and type of two variables are equal
+document.write("<br>");
+document.write("2" === 2 );//utilise === to check if the value and type of two variables are equal
+document.write("<br>");
+document.write("2" === "1" );//utilise === to check if the value and type of two variables are equal
+document.write("<br>");
+document.write("2" === "2" && 2==2);//utilise && (AND) operator to determine the logic between values or variable
+document.write("<br>");
+document.write("2" === "2" && 1==2);//utilise && (AND) operator to determine the logic between values or variable
+document.write("<br>");
+document.write("2" === "2" || 1==2);//utilise || (OR) operator to determine the logic between values or variable
+document.write("<br>");
+document.write("2" === "1" || 1==2);//utilise || (OR) operator to determine the logic between values or variable
+document.write("<br>");
+document.write(!("2" === "2"));//utilise ! (NOT) operator to determine the logic between values or variable
+document.write("<br>");
+document.write(!("2" === "1"));//utilise ! (NOT) operator to determine the logic between values or variable
